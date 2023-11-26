80438407f91be819f567d629ed7b1db7bafd2ed1
Kevin <kevinolongo9@gmail.com>
2023-11-26 22:18:18 +0300

    Convert data types

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) ==1:
+        return items[0]
+    elif len(items) == 2:
+        return f"{items[0]} and {items[1]}"
+    else:
+        seperated_by_comma = ", ".join(items[:-1])
+        return f"{seperated_by_comma}, and {items[-1]}"
